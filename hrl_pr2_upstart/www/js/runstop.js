1692fbc055e0f4a76a5bb97594a4337724751ec9
Phillip Grice <phillip.grice@gatech.edu>
2016-07-20 15:38:41 -0400

    add web interface for run-stop, and launch file to bring up at boot

---
+++ hrl_pr2_upstart/www/js/runstop.js
@@ -0,0 +1,37 @@
+(function ($, ROSLIB){
+    var ROBOT = window.location.host.split(':')[0];//Use localhost when serving website directly from robot 
+    var PORT = '1820';//Must match port on which rosbridge is being served
+    var ros = new ROSLIB.Ros({url: 'ws://'+ ROBOT + ':'+ PORT});
+    ros.on('close', function(e) {
+       console.log("Disconnected or Can't Connect to " + ROBOT + ":"+ PORT + ".");
+       $(body).html("<h1>Lost Connection to Robot<h1>").css('background-color','gray');
+      }
+    );
+
+    ros.on('error', function(e) {
+      console.log("Rosbridge Connection Error!");
+      $(body).html("<h1>Lost Connection to Robot<h1>").css('background-color','gray');
+      }
+    );
+
+    ros.on('connection', function(e) {
+        console.log("Connected to " + ROBOT + ".");
+         var emulatorService = new ROSLIB.Service({
+            ros: ros,
+            name:'/emulate_runstop',
+            serviceType: 'hrl_pr2_upstart/SetRunStop'}
+        );
+
+        var stop = function(event) {
+            emulatorService.callService({'stop': true, 'start': false});
+        };
+
+        var reset = function(event) {
+            emulatorService.callService({'stop': false, 'start': true});
+        };
+        
+        $('#stop-button').button().on('mousedown.runstop', stop);
+        $('#reset-button').button().on('click.runstop', reset);
+
+    });
+}(jQuery, ROSLIB));
