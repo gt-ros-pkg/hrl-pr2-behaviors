20fbfd0e5c30ab85c54ca110c8e4d3cd29abdd03
Phillip Grice <phillip.grice@gatech.edu>
2016-08-01 11:19:14 -0400

    tested + fixed joint limit watchdog.  Stops and resets reliably after very brief testing

--- hrl_pr2_upstart/src/hrl_pr2_upstart/joint_limit_watchdog.py
+++ hrl_pr2_upstart/src/hrl_pr2_upstart/joint_limit_watchdog.py
@@ -28,36 +28,41 @@ class JointLimitWatchdog(object):
         self.emulate_runstop_service = rospy.ServiceProxy('/emulate_runstop', SetRunStop, persistent=True)
         self.motor_status_sub = rospy.Subscriber('/pr2_ethercat/motors_halted', Bool, self.motor_state_cb)
         self.r_arm_state_sub = rospy.Subscriber('/r_arm_controller/state', JointTrajectoryControllerState, self.joint_state_cb, 'right_arm')
-        self.l_arm_state_sub = rospy.Subscriber('/r_arm_controller/state', JointTrajectoryControllerState, self.joint_state_cb, 'left_arm')
+        self.l_arm_state_sub = rospy.Subscriber('/l_arm_controller/state', JointTrajectoryControllerState, self.joint_state_cb, 'left_arm')
         self.head_state_sub = rospy.Subscriber('/head_traj_controller/state', JointTrajectoryControllerState, self.joint_state_cb, 'head')
+        rospy.loginfo("[%s] Ready", rospy.get_name())
 
-    def arm_state_cb(self, msg, limb):
-        if self.motors_halted or self.reset_timer is not None:
-            # Expect joints to violate soft limits when motors are halted
+    def joint_state_cb(self, msg, limb):
+        if self.motors_halted:
             return
         joints = np.array(msg.actual.positions)
         limits = self.limits[limb]
         if any(joints < limits[0]) or any(joints > limits[1]):
-            self.halt_motors()
-            rospy.logwarn("[%s] Joints outside soft limits.", rospy.get_name())
+            rospy.logwarn("[%s] Joints in %s outside soft limits.", rospy.get_name(), limb)
+#            print limits[1]
+#            print joints
+#            print limits[0]
             self.outside_limits = True
+            if self.reset_timer is None:  # Expect joints to violate soft limits when motors are halted
+                self.halt_motors()
         else:
             self.outside_limits = False
 
     def motor_state_cb(self, state_msg):
         if state_msg.data:
-            if self.reset_timer is not None:
+            if self.reset_timer is not None:  # Clear any running reset timers, since we're stopped again
                 self.reset_timer.shutdown()
                 self.reset_time = None
-            if not self.motors_halted:
-                self.motors_halted = True
-        elif self.motors_halted:  # Now not halted, but were
+        if not state_msg.data and self.motors_halted:  # Now not halted, but were, so we've reset.  Create timer to check limits after reset
             self.reset_timer = rospy.Timer(rospy.Duration(4), self.check_reset, oneshot=True)
+        self.motors_halted = state_msg.data  # Always update to latest state
 
-    def check_reset(self):
+    def check_reset(self, event):
         if self.outside_limits:
             rospy.logwarn("[%s] Joints outside soft limits after reset.", rospy.get_name())
             self.halt_motors()
+        else:
+            rospy.loginfo("[%s] Joints within limits after reset.", rospy.get_name())
         self.reset_timer = None
 
     def halt_motors(self):
