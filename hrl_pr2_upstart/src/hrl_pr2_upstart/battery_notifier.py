1b31405edd64576896a2869884ef6644fc6c8191
Phillip Grice <phillip.grice@gatech.edu>
2016-08-05 16:59:16 -0400

    further work on battery notifier node

--- hrl_pr2_upstart/src/hrl_pr2_upstart/battery_notifier.py
+++ hrl_pr2_upstart/src/hrl_pr2_upstart/battery_notifier.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python
 
+import subprocess
+import numpy as np
+
 import rospy
 
 from pr2_msgs.msg import BatteryServer2
 from sound_play.msg import SoundRequest
 
 BATTERY_COUNT = 4
+STALE_TIMEOUT = 300 # 300s = 5 minutes
 
 
 class BatteryNotifier(object):
     def __init__(self):
-        self.low_battery_warn_pcts = []
+        self.low_battery_warn_pcts = [100, 75, 50, 20, 10, 5]
+        self.low_battery_warn_pcts.sort()
+        self.low_battery_warn_pcts.reverse()  # Keep in consistent high-to-low order
+        self.low_battery_level_warned = None
 
-        self.sound_request_pub = rospy.Publisher('/robotsound', SoundRequest, queue=3)
+        self.sound_request_pub = rospy.Publisher('/robotsound', SoundRequest, queue_size=3)
         self.charge_pct = [None]*BATTERY_COUNT
         self.time_left = [None]*BATTERY_COUNT
         self.last_update = [None]*BATTERY_COUNT
+        self.power_present_list = [None]*BATTERY_COUNT*4
+        self.power_present = None
         self.battery_state_sub = rospy.Subscriber('/battery/server2', BatteryServer2, self.battery_state_cb)
         while any([state is None for state in self.charge_pct]):
             rospy.sleep(4)
@@ -27,16 +36,26 @@ class BatteryNotifier(object):
             rospy.loginfo("[%s] Received data on battery %d", rospy.get_name(), msg.id)
         self.charge_pct[msg.id] = msg.average_charge
         self.time_left[msg.id] = msg.time_left
-        self.last_update[msg.id] = msg.last_system_update
+
+        # Warn for low battery percentages
+        avg_pct = np.mean(self.charge_pct)
+        if avg_pct in self.low_battery_warn_pcts
+        for i, level in enumerate(self.low_battery_warn_pcts):
+            if avg_pct < level:
+                warn_level  = level_idx
+
+        # Keep track of plug state, beep for power added or removed
+        self.power_present_list[msg.id*4:msg.id*4+4] = [batt.power_present for batt in msg.battery]
+        power_present_now = bool(sum([x for x in self.power_present_list if x is not None]) > len(self.power_present_list)/2)
+        if self.power_present and not power_present_now:
+            subprocess.call(['beep', '-l', '300', '-f', '1000', '-n', '-l', '500', '-f', '600'])
+        elif not self.power_present and power_present_now:
+            subprocess.call(['beep', '-l', '300', '-f', '600', '-n', '-l', '500', '-f', '1000'])
+        self.power_present = power_present_now
 
     def check_status(self):
         self.average_pct = np.mean(self.charge_pct)
         self.average_time = np.mean(self.charge_pct)
-
-        if self.average_pct <
-
-
-
 
 
 def main():
@@ -45,5 +64,5 @@ def main():
     rospy.spin()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
